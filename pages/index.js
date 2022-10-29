b3f871fee7eafc3e197230b9f97e8b0f98dc69f2
Na jewon <80616120+jewonr@users.noreply.github.com>
2022-10-29 11:23:41 +0900

    update text

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Jewon. I'm sunrin internet high school student. I want to be a front-end engineer.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a risus posuere.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
